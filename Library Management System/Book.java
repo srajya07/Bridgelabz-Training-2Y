4b60569523973a4d57aa4c0903c90af1790514e4
srajya07 <srajya066gmail.com>
2026-09-22 12:37:02 +0530

    [Rajya Vardhan Singh]Add.Library Management System Problems

---
+++ Library Management System/Book.java
@@ -0,0 +1,13 @@
+public class Book {
+        int bookId;
+        String title;
+        String author;
+        double price;
+
+        public Book(int bookId, String title, String author, double price) {
+            this.bookId = bookId;
+            this.title = title;
+            this.author = author;
+            this.price = price;
+        }
+    }
