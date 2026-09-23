2092aa275babeb1f9ce7219b8094244947597d8d
srajya07 <srajya066gmail.com>
2026-09-23 23:58:50 +0530

    [Rajya Vardhan Singh]Add.Sting Leet Code Problems

---
+++ String/LC_709.java
@@ -0,0 +1,17 @@
+package String;
+
+public class LC_709 {
+    public String toLowerCase(String s) {
+        String result = "";
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(65 <= ch && ch <= 90){
+                result += (char)(ch + 32);
+            }
+            else{
+                result += ch;
+            }
+        }
+        return result;
+    }
+}
